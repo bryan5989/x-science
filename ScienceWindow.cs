902dec80e126545ac00c6af511997d78c0abc2ed
Bodrick <bodricklight@gmail.com>
2014-10-08 22:15:24 +0100

    ScienceWindow: Compacted.

--- ScienceWindow.cs
+++ ScienceWindow.cs
@@ -10,7 +10,7 @@ namespace ScienceChecklist {
 
 		public ScienceWindow () {
 			_logger = new Logger(this);
-			_rect = new Rect(0, 0, 600, 500);
+			_rect = new Rect(40, 40, 500, 400);
 			_scrollPos = new Vector2();
 			_filter = new ExperimentFilter();
 			_progressTexture = new Texture2D(13, 13, TextureFormat.ARGB32, false);
@@ -20,6 +20,7 @@ namespace ScienceChecklist {
 			_emptyTexture = new Texture2D(1, 1, TextureFormat.ARGB32, false);
 			_emptyTexture.SetPixels(new[] { Color.clear });
 			_emptyTexture.Apply();
+			_fullMode = false;
 		}
 
 		#region PROPERTIES
@@ -39,6 +40,7 @@ namespace ScienceChecklist {
 			GUI.skin = GameObject.Instantiate(HighLogic.Skin) as GUISkin;
 
 			_rect = GUILayout.Window(_windowId, _rect, DrawControls, "[x] Science!");
+
 			GUI.skin = oldSkin;
 		}
 
@@ -63,29 +65,46 @@ namespace ScienceChecklist {
 			ProgressBar (completePercent, GUILayout.ExpandWidth (true), GUILayout.Height(13));
 
 			GUILayout.BeginHorizontal();
+
+			var oldPadding = GUI.skin.label.padding;
+			GUI.skin.label.padding = new RectOffset(0, 0, 4, 0);
 			GUILayout.Label(new GUIContent(string.Format("{0}/{1} complete.", _filter.CompleteCount, _filter.DisplayExperiments.Count)), GUILayout.Width(150));
 			GUILayout.Label(new GUIContent("Search:"));
+			GUI.skin.label.padding = oldPadding;
+
 			_filter.Text = GUILayout.TextField(_filter.Text, GUILayout.ExpandWidth(true));
+
+			//oldPadding = GUI.skin.button.padding;
+			//GUI.skin.button.padding = new RectOffset(1, 1, 4, 1);
+			
+			if (GUILayout.Button(new GUIContent("x"), GUILayout.Width(25), GUILayout.Height(23))) {
+				_filter.Text = string.Empty;
+			}
+
+			//GUI.skin.button.padding = oldPadding;
 			GUILayout.EndHorizontal();
 
 			GUILayout.BeginHorizontal();
-			GUILayout.BeginVertical(GUILayout.Width(150));
-			GUILayout.FlexibleSpace();
-			_filter.DisplayMode = (DisplayMode) GUILayout.SelectionGrid((int) _filter.DisplayMode, new[] {
-				new GUIContent("Active vessel"),
-				new GUIContent("All unlocked"),
-				new GUIContent("All"),
-			}, 1, GUILayout.ExpandWidth(true));
-			_filter.HideComplete = GUILayout.Toggle(_filter.HideComplete, new GUIContent("Hide complete"));
-			GUILayout.EndVertical();
 
 			_scrollPos = GUILayout.BeginScrollView(_scrollPos, GUI.skin.scrollView);
 
 			foreach (var experiment in _filter.DisplayExperiments.Where (x => !_filter.HideComplete || !x.IsComplete)) {
 				DrawExperiment(experiment);
 			}
+
 			GUILayout.FlexibleSpace();
 			GUILayout.EndScrollView();
+			GUILayout.EndHorizontal();
+
+			GUILayout.BeginHorizontal();
+
+			_filter.DisplayMode = (DisplayMode) GUILayout.SelectionGrid((int) _filter.DisplayMode, new[] {
+				new GUIContent("On vessel"),
+				new GUIContent("Unlocked"),
+				new GUIContent("All"),
+			}, 3);
+			_filter.HideComplete = GUILayout.Toggle(_filter.HideComplete, new GUIContent("Hide complete"), GUILayout.ExpandWidth(true));
+
 			GUILayout.EndHorizontal();
 			GUILayout.EndVertical();
 			GUI.DragWindow();
@@ -116,6 +135,7 @@ namespace ScienceChecklist {
 
 		private Rect _rect;
 		private Vector2 _scrollPos;
+		private bool _fullMode;
 
 		private Texture2D _progressTexture;
 		private readonly Texture2D _emptyTexture;
