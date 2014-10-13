a580c616a7d5035e3d4a42614abfc107433afe03
Bodrick <bodricklight@gmail.com>
2014-10-13 23:38:53 +0100

    Started making the filter buttons prettier.

--- ScienceWindow.cs
+++ ScienceWindow.cs
@@ -12,14 +12,16 @@ namespace ScienceChecklist {
 			_rect = new Rect(40, 40, 500, 400);
 			_scrollPos = new Vector2();
 			_filter = new ExperimentFilter();
-			_progressTexture = new Texture2D(13, 13, TextureFormat.ARGB32, false);
-			var iconStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("ScienceChecklist.scienceProgress.png").ReadToEnd();
-			_progressTexture.LoadImage(iconStream);
-			_progressTexture.Apply();
-			_completeTexture = new Texture2D(13, 13, TextureFormat.ARGB32, false);
-			iconStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("ScienceChecklist.scienceComplete.png").ReadToEnd();
-			_completeTexture.LoadImage(iconStream);
-			_completeTexture.Apply();
+			_progressTexture = TextureHelper.FromResource("ScienceChecklist.scienceProgress.png", 13, 13);
+			_completeTexture = TextureHelper.FromResource("ScienceChecklist.scienceComplete.png", 13, 13);
+			_currentSituationTexture = TextureHelper.FromResource("ScienceChecklist.icons.currentSituation.png", 25, 31);
+			_currentVesselTexture = TextureHelper.FromResource("ScienceChecklist.icons.currentVessel.png", 25, 31);
+			_unlockedTexture = TextureHelper.FromResource("ScienceChecklist.icons.unlocked.png", 25, 31);
+			_allTexture = TextureHelper.FromResource("ScienceChecklist.icons.all.png", 25, 31);
+			_hideCompleteTexture = TextureHelper.FromResource("ScienceChecklist.icons.hideComplete.png", 25, 31);
+			_showCompleteTexture = TextureHelper.FromResource("ScienceChecklist.icons.showComplete.png", 25, 31);
+			_searchTexture = TextureHelper.FromResource("ScienceChecklist.icons.search.png", 25, 31);
+			_clearSearchTexture = TextureHelper.FromResource("ScienceChecklist.icons.clearSearch.png", 25, 31);
 			_emptyTexture = new Texture2D(1, 1, TextureFormat.ARGB32, false);
 			_emptyTexture.SetPixels(new[] { Color.clear });
 			_emptyTexture.Apply();
@@ -130,20 +132,21 @@ namespace ScienceChecklist {
 
 			var oldPadding = GUI.skin.label.padding;
 			GUI.skin.label.padding = new RectOffset(0, 0, 4, 0);
-			GUILayout.Label(new GUIContent(string.Format("{0}/{1} complete.", _filter.CompleteCount, _filter.TotalCount)), GUILayout.Width(150));
-			GUILayout.Label(new GUIContent("Search:"));
+			GUILayout.Label(string.Format("{0}/{1} complete.", _filter.CompleteCount, _filter.TotalCount), GUILayout.Width(150));
+			GUI.skin.label.padding = new RectOffset(0, 0, 0, 0);
+			GUILayout.Label(new GUIContent(_searchTexture));
 			GUI.skin.label.padding = oldPadding;
 
 			_filter.Text = GUILayout.TextField(_filter.Text, GUILayout.ExpandWidth(true));
 
-			if (GUILayout.Button(new GUIContent("x"), GUILayout.Width(25), GUILayout.Height(23))) {
+			if (GUILayout.Button(new GUIContent(_clearSearchTexture), GUILayout.Width(25), GUILayout.Height(23))) {
 				_filter.Text = string.Empty;
 			}
 
 			GUILayout.EndHorizontal();
 
 			if (_filter.CurrentSituation != null) {
-				GUILayout.Label(new GUIContent("Currently " + _filter.CurrentSituation.Description + "."));
+				GUILayout.Label("Currently " + _filter.CurrentSituation.Description + ".");
 			}
 
 			_scrollPos = GUILayout.BeginScrollView(_scrollPos, GUI.skin.scrollView);
@@ -165,12 +168,17 @@ namespace ScienceChecklist {
 			GUILayout.BeginHorizontal();
 
 			_filter.DisplayMode = (DisplayMode) GUILayout.SelectionGrid((int) _filter.DisplayMode, new[] {
-				new GUIContent("Right now"),
-				new GUIContent("On vessel"),
-				new GUIContent("Unlocked"),
-				new GUIContent("All"),
+				new GUIContent(_currentSituationTexture, "Show experiments available right now"),
+				new GUIContent(_currentVesselTexture, "Show experiments available on this vessel"),
+				new GUIContent(_unlockedTexture, "Show all unlocked experiments"),
+				new GUIContent(_allTexture, "Show all experiments"),
 			}, 4);
-			_filter.HideComplete = GUILayout.Toggle(_filter.HideComplete, new GUIContent("Hide complete"), GUILayout.ExpandWidth(true));
+
+			GUILayout.FlexibleSpace();
+			var toggleComplete = GUILayout.Button(new GUIContent(_filter.HideComplete ? _hideCompleteTexture : _showCompleteTexture, _filter.HideComplete ? "Currently hiding completed experiments" : "Currently showing completed experiments"), GUILayout.ExpandWidth(false));
+			if (toggleComplete) {
+				_filter.HideComplete = !_filter.HideComplete;
+			}
 
 			GUILayout.EndHorizontal();
 			GUILayout.EndVertical();
@@ -188,7 +196,7 @@ namespace ScienceChecklist {
 				y = rect.y + 3,
 			};
 
-			GUI.Label(labelRect, new GUIContent(exp.Description), _labelStyle);
+			GUI.Label(labelRect, exp.Description, _labelStyle);
 			ProgressBar(progressRect, exp.CompletedScience, exp.TotalScience, true);
 		}
 
@@ -205,7 +213,7 @@ namespace ScienceChecklist {
 				var labelRect = new Rect(rect) {
 					y = rect.y - 1,
 				};
-				GUI.Label(labelRect, new GUIContent(string.Format("{0:0.#}  /  {1:0.#}", curr, total)), curr == 0 ? _emptyLabelStyle : curr >= total ? _completeLabelStyle : _progressLabelStyle);
+				GUI.Label(labelRect, string.Format("{0:0.#}  /  {1:0.#}", curr, total), curr == 0 ? _emptyLabelStyle : curr >= total ? _completeLabelStyle : _progressLabelStyle);
 			}
 		}
 
@@ -223,6 +231,15 @@ namespace ScienceChecklist {
 		private readonly Texture2D _progressTexture;
 		private readonly Texture2D _completeTexture;
 		private readonly Texture2D _emptyTexture;
+		private readonly Texture2D _currentSituationTexture;
+		private readonly Texture2D _currentVesselTexture;
+		private readonly Texture2D _unlockedTexture;
+		private readonly Texture2D _allTexture;
+		private readonly Texture2D _hideCompleteTexture;
+		private readonly Texture2D _showCompleteTexture;
+		private readonly Texture2D _searchTexture;
+		private readonly Texture2D _clearSearchTexture;
+
 		private readonly ExperimentFilter _filter;
 		private readonly Logger _logger;
 		private readonly int _windowId = UnityEngine.Random.Range(0, int.MaxValue);
---
+++ TextureHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+
+namespace ScienceChecklist {
+	internal static class TextureHelper {
+		public static Texture2D FromResource (string resource, int width, int height) {
+			var tex = new Texture2D(13, 13, TextureFormat.ARGB32, false);
+			var iconStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resource).ReadToEnd();
+			tex.LoadImage(iconStream);
+			tex.Apply();
+			return tex;
+		}
+	}
+}
